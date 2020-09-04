b8c046931e2919b83d4dbbcc012a2b4dd5af8301
Andy <Achen725.cfl@gmail.com>
2020-09-04 13:49:46 -0400

    Added years at cmu

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-    	System.out.println("Hello World");
+    	System.out.println("Hello World - CMU 2 years so far going into ");
     }
 }
 
